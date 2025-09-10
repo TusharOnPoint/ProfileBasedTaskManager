02505227e810caaae99aed6f1b2f5f8496857dde
TusharOnPoint <tushar23das@gmail.com>
2025-09-10 20:28:41 +0600

    Task controller class implemented.

---
+++ src/main/java/com/tushar/taskmanagerapp/service/TaskService.java
@@ -0,0 +1,17 @@
+package com.tushar.taskmanagerapp.service;
+
+import java.util.List;
+
+import com.tushar.taskmanagerapp.dto.Response;
+import com.tushar.taskmanagerapp.dto.TaskRequest;
+import com.tushar.taskmanagerapp.model.Task;
+
+public interface TaskService {
+    Response<Task> createTask(TaskRequest taskRequest);
+    Response<List<Task>> getAllMyTasks();
+    Response<Task> getTaskById(Long id);
+    Response<Task> updateTask(TaskRequest taskRequest);
+    Response<Void> deleteTask(Long id);
+    Response<List<Task>> getMyTasksByCompletedStatus(boolean completed);
+    Response<List<Task>> getMyTasksByPriority(String priority);
+}
---
+++ src/main/java/com/tushar/taskmanagerapp/service/implementation/EmailServiceImpl.java
@@ -0,0 +1,31 @@
+package com.tushar.taskmanagerapp.service.implementation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.tushar.taskmanagerapp.service.EmailService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    @Override
+    public void send(String to, String subject, String body) {
+    SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText(body);
+        mailSender.send(msg);
+    }
+    
+}
